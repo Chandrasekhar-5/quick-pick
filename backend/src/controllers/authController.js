eddb94e91d3d76fb16337de73fe828deb64215b7
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-02-25 15:09:20 +0530

    authController

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,39 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password, role, college} = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role,
+            college
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id)
+            });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { registerUser };
