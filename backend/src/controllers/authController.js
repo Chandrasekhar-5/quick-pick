09ebf048ebf74699bfb325a6036c5464e9d96326
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-02-26 13:20:46 +0530

    add login user

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -36,4 +36,26 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (user && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id)
+            });
+        } else {
+            res.status(401).json({ message: "Invalid email or password" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { registerUser, loginUser };
