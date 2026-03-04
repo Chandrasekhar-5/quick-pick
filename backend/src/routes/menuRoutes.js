d29328e900955e456c3371c30f4f8a00974717f9
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-03-04 21:55:18 +0530

    swagger for menu routes

--- backend/src/routes/menuRoutes.js
+++ backend/src/routes/menuRoutes.js
@@ -3,7 +3,47 @@ const router = express.Router();
 const { addMenuItem, getMenuItemsByVendor } = require('../controllers/menuController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
+
+/**
+ * @swagger
+ * /api/menu:
+ *   post:
+ *     summary: Add menu item
+ *     tags: [Menu]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             name: Veg Biryani
+ *             price: 80
+ *             isVeg: true
+ *     responses:
+ *       201:
+ *         description: Menu item created
+ */
+
 router.post('/', protect, authorize('vendor'), addMenuItem);
+
+/**
+ * @swagger
+ * /menu/{vendorId}:
+ *   get:
+ *     summary: Get menu items for a vendor
+ *     tags: [Menu]
+ *     parameters:
+ *       - in: path
+ *         name: vendorId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Vendor menu
+ */
+
 router.get('/:vendorId', getMenuItemsByVendor);
 
 module.exports = router;
