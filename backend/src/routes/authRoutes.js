f51f40b5a72da8f2518b267edcf1822091c55e95
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-03-04 15:11:59 +0530

    swagger for register and login

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,49 @@ const router = express.Router();
 const { registerUser, loginUser, getMe } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             name: John Doe
+ *             email: praveen@praveen.com
+ *             password: praveen_praveen
+ *             role: student
+ *             college: collegeId
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ */
+
+
 router.post("/register", registerUser);
+
+
+/**
+ * @swagger
+ * /auth.login:
+ *  post:
+ *    summary: Login user
+ *    tags: [Auth]
+ *    requestBody:
+ *        required: true
+ *        content:
+ *           application/json:
+ *              example:
+ *                email: praveen@praveen.com
+ *                password: praveen_praveen
+ *    responses:
+ *      200:
+ *        description: User logged in successfully
+ */
 router.post("/login", loginUser);
 router.get("/me",protect, getMe);
 
