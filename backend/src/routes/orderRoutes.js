dd5b645a328b205ebb7547a4b6069adeaae0b161
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-03-04 15:38:08 +0530

    swagger for orders

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const { placeOrder, getMyOrders, getVendorOrders, updateOrderStatus } = require('../controllers/orderController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
+/**
+ * @swagger
+ * /orders:
+ *   post:
+ *     summary: Place an order
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           example:
+ *             vendorId: vendorId
+ *             items:
+ *               - menuItem: menuItemId
+ *                 quantity: 2
+ *     responses:
+ *       201:
+ *         description: Order created
+ */
+
 router.post('/', protect, placeOrder);
 router.get('/myOrders', protect, getMyOrders);
 
