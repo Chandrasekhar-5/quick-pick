26e4fe5078bed210b57c331fc2838894b7525e49
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-03-09 19:03:09 +0530

    made sure the vendor can login (but there is a problem in registration)

--- frontend/vendor-frontend/src/services/api.ts
+++ frontend/vendor-frontend/src/services/api.ts
@@ -6,7 +6,7 @@ const API = axios.create({
 
 API.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("studentToken");
+    const token = localStorage.getItem("vendorToken");
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
