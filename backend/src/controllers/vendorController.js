a9a564ddcc344321106b07b1a8eb2f5908b5dae8
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-03-11 19:16:51 +0530

    feat: updated vendor controller y implemnting logic for getVendorByOwner, getVendorById, updateVendor

--- backend/src/controllers/vendorController.js
+++ backend/src/controllers/vendorController.js
@@ -2,7 +2,7 @@ const Vendor = require("../models/Vendor");
 
 const createVendor = async (req, res) => {
     try {
-        const { name, description } = req.body;
+        const { name, description, phone, openingTime, closingTime, logo, address } = req.body;
 
         const existingShop = await Vendor.findOne({ ownerId: req.user._id});
         if (existingShop) {
@@ -12,6 +12,11 @@ const createVendor = async (req, res) => {
         const vendor = await Vendor.create({
             name,
             description,
+            phone,
+            openingTime,
+            closingTime,
+            logo,
+            address,
             collegeId: req.user.college,
             ownerId: req.user._id
         });
@@ -30,4 +35,46 @@ const getVendors = async (req, res) => {
     }
 };
 
-module.exports = { createVendor, getVendors };
+const getVendorByOwner = async (req, res) => {
+    try {
+        const vendor = await Vendor.findOne({ ownerId: req.params.ownerId });
+        if (!vendor) {
+            return res.status(404).json({ message: "Vendor not found" });
+        }
+        res.status(200).json(vendor);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getVendorById = async (req, res) => {
+    try {
+        const vendor = await Vendor.findById(req.params.id);
+        if (!vendor) {
+            return res.status(404).json({ message: "Vendor not found" });
+        }
+        res.status(200).json(vendor);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const updateVendor = async (req, res) => {
+    try {
+        const vendor = await Vendor.findById(req.params.id);
+        if (!vendor) {
+            return res.status(404).json({ message: "Vendor not found" });
+        }
+
+        if (vendor.ownerId.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Not authorized to update this vendor" });
+        }
+
+        const updateVendor = await Vendor.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        res.status(200).json(updateVendor);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { createVendor, getVendors, getVendorByOwner, getVendorById, updateVendor };
