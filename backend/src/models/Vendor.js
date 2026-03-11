c1d005eec6846ad8184582f39be3303d809f1075
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-03-11 19:15:29 +0530

    feat: updated vendor schema with more deatils

--- backend/src/models/Vendor.js
+++ backend/src/models/Vendor.js
@@ -19,6 +19,26 @@ const vendorSchema = new mongoose.Schema(
             ref: 'User',
             required: true
         },
+        phone: {
+            type: String,
+            required: [true, 'Please add a phone number']
+        },
+        openingTime: {
+            type: String,
+            default: '8:00 AM'
+        },
+        closingTime: {
+            type: String,
+            default: '5:00 PM'
+        },
+        logo: {
+            type: String,
+            default: 'https://www.freeiconspng.com/uploads/no-image-icon-9.png'
+        },
+        address: {
+            type: String,
+            required: [true, 'Please add an address']
+        },
         isOpen: {
             type: Boolean,
             default: true
